3275f39e9213b57e656c7e32e7bfb1c358cce949
asosunoff <al.sosunoff@gmail.com>
2021-03-26 22:13:25 +0300

    code comment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import bridge from "@vkontakte/vk-bridge";
+/* import bridge from "@vkontakte/vk-bridge"; */
 import App from "./App";
-// Firebase App (the core Firebase SDK) is always required and must be listed first
 import firebase from "firebase/app";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
 import "firebase/analytics";
-// Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
 
@@ -27,19 +22,18 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 // Init VK  Mini App
-bridge.send("VKWebAppInit");
+/* bridge.send("VKWebAppInit"); */
 
 /*  */
-const db = firebase.firestore();
+/* const db = firebase.firestore();
 
 db.collection("desks")
   .get()
   .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.id}`);
-      console.log(doc.data());
+      console.log(doc.id, doc.data());
     });
-  });
+  }); */
 /*  */
 
 ReactDOM.render(<App />, document.getElementById("root"));
