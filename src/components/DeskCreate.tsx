938cb863bf6c2808d8dc0afe7a8b644f0c5cf859
asosunoff <al.sosunoff@gmail.com>
2021-03-27 12:05:02 +0300

    added form create desk

--- src/components/DeskCreate.tsx
+++ src/components/DeskCreate.tsx
@@ -1,14 +1,56 @@
-import React from "react";
-import { Button, Div } from "@vkontakte/vkui";
-import { Icon24Add } from "@vkontakte/icons";
+import React, { useState } from "react";
+import { Button, Div, FormItem, FormLayout, FormLayoutGroup, Input } from "@vkontakte/vkui";
+import { Icon24Add, Icon24DeleteOutline } from "@vkontakte/icons";
+
+type Modes = "button" | "form";
 
 const DeskCreate: React.FC = () => {
+  const [mode, setMode] = useState<Modes>("button");
+  const [name, setName] = useState("");
+
+  if (mode === "button") {
+    return (
+      <Div>
+        <Button size="l" stretched before={<Icon24Add />} onClick={() => setMode("form")}>
+          Создать доску
+        </Button>
+      </Div>
+    );
+  }
+
   return (
-    <Div>
-      <Button size="l" stretched before={<Icon24Add />}>
-        Создать доску
-      </Button>
-    </Div>
+    <FormLayout>
+      <FormItem top="Наименование доски">
+        <Input
+          autoFocus
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+      </FormItem>
+
+      <FormLayoutGroup mode="horizontal">
+        <FormItem>
+          <Button size="l" stretched before={<Icon24Add />}>
+            Создать доску
+          </Button>
+        </FormItem>
+
+        <FormItem>
+          <Button
+            size="l"
+            stretched
+            onClick={() => setMode("button")}
+            before={<Icon24DeleteOutline />}
+            mode="tertiary"
+            color="red"
+          >
+            Отменить
+          </Button>
+        </FormItem>
+      </FormLayoutGroup>
+    </FormLayout>
   );
 };
 
