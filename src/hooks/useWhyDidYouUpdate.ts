b0744822ae2d67a378bd25ac7464aded8e243fe3
asosunoff <al.sosunoff@gmail.com>
2021-04-29 19:10:41 +0300

    added useWhyDidYouUpdate

---
+++ src/hooks/useWhyDidYouUpdate.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+export const useWhyDidYouUpdate = (name: string, props: Record<string, any>) => {
+  const previousProps = useRef<Record<string, any>>({});
+
+  useEffect(() => {
+    if (previousProps.current) {
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+
+      const changesObj: Record<string, any> = {};
+
+      allKeys.forEach((key) => {
+        if (previousProps.current[key] !== props[key]) {
+          changesObj[key] = {
+            from: previousProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+
+      if (Object.keys(changesObj).length) {
+        console.log("[why-did-you-update]", name, changesObj);
+      }
+    }
+
+    previousProps.current = props;
+  });
+};
