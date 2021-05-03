82549341a32cd9dbc5315dc9493e61caa6dc3008
asosunoff <al.sosunoff@gmail.com>
2021-05-03 12:09:49 +0300

    added useSelector and useDispatch to cards

--- src/features/columns/components/cards/index.tsx
+++ src/features/columns/components/cards/index.tsx
@@ -1,22 +1,44 @@
-import { connect } from "react-redux";
-
-import { Cards } from "./cards";
-import { RootState } from "../../../../app/redux/reducers";
+import React, { useEffect } from "react";
+import { List } from "@vkontakte/vkui";
+import { Card } from "../card";
+import { CreateForm } from "../../../../components/create-form";
 import { fetchCards, addedCard } from "../../actions/cardActions";
-import * as I from "./interfaces";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../../app/redux/reducers";
+import { ICard } from "../../interfaces/ICard";
 
-const mapStateToProps = (
-  { columns }: RootState,
-  { columnId }: { columnId: string }
-): I.StateProps => ({
-  cards: columns?.column.list[columnId].cards ?? [],
-});
+interface CardsProps {
+  columnId: string;
+}
 
-const mapDispatchToProps = {
-  fetchCards,
-  addedCard,
+const Cards: React.FC<CardsProps> = ({ columnId }) => {
+  const dispatch = useDispatch();
+
+  const cards = useSelector<RootState, ICard[]>(
+    ({ columns }) => columns?.column.list[columnId].cards ?? []
+  );
+
+  useEffect(() => {
+    dispatch(fetchCards(columnId));
+  }, [columnId, dispatch]);
+
+  return (
+    <>
+      <List>
+        {cards.map((card) => (
+          <Card key={card.id} card={card} />
+        ))}
+      </List>
+
+      <CreateForm
+        onSubmit={async (name) => {
+          await dispatch(addedCard(columnId, name));
+        }}
+        buttonName="Создать карточку"
+        placeholder="введите название карточки"
+      />
+    </>
+  );
 };
 
-const result = connect(mapStateToProps, mapDispatchToProps)(Cards);
-
-export { result as Cards };
+export { Cards };
