6e38cb077453eabc67b8383dad0b378fc02b9fd6
asosunoff <al.sosunoff@gmail.com>
2021-05-03 12:01:30 +0300

    added useDispatch to card

--- src/features/columns/components/card/index.tsx
+++ src/features/columns/components/card/index.tsx
@@ -1,16 +1,46 @@
-import { connect } from "react-redux";
-
-import { Card } from "./card";
-import { RootState } from "../../../../app/redux/reducers";
+import React, { useCallback } from "react";
+import { Cell } from "@vkontakte/vkui";
+import { useAlertContext } from "../../../../context/alert-context";
+import { ICard } from "../../interfaces/ICard";
 import { removeCard } from "../../actions/cardActions";
-import * as I from "./interfaces";
+import { useDispatch } from "react-redux";
 
-const mapStateToProps = ({}: RootState): I.StateProps => ({});
+interface CardProps {
+  card: ICard;
+}
 
-const mapDispatchToProps = {
-  removeCard,
+const Card: React.FC<CardProps> = ({ card }) => {
+  const dispatch = useDispatch();
+
+  const { setPopoutHandler, clearPopoutHandler } = useAlertContext();
+
+  const question = useCallback(() => {
+    setPopoutHandler({
+      header: "Внимание",
+      text: `Вы уверены в удалении карточки ${card.name}`,
+      actions: [
+        {
+          title: "Да",
+          mode: "destructive",
+          autoclose: true,
+          action: () => dispatch(removeCard(card)),
+        },
+        {
+          title: "Передумал",
+          mode: "cancel",
+          autoclose: true,
+        },
+      ],
+      actionsLayout: "vertical",
+      onClose: clearPopoutHandler,
+    });
+  }, [setPopoutHandler, card, clearPopoutHandler, dispatch]);
+
+  return (
+    <Cell removable onRemove={question}>
+      {card.name}
+    </Cell>
+  );
 };
 
-const result = connect(mapStateToProps, mapDispatchToProps)(Card);
-
-export { result as Card };
+export { Card };
