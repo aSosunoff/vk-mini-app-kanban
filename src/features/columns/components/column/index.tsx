a6e65a03fa9f72aa05ca886380291a34bc4a4c6b
asosunoff <al.sosunoff@gmail.com>
2021-05-03 11:59:21 +0300

    added useDispatch

--- src/features/columns/components/column/index.tsx
+++ src/features/columns/components/column/index.tsx
@@ -1,16 +1,54 @@
-import { connect } from "react-redux";
-
-import { Column } from "./column";
-import { RootState } from "../../../../app/redux/reducers";
-import * as I from "./interfaces";
+import React, { useCallback } from "react";
+import { Group, Header } from "@vkontakte/vkui";
+import { Icon16Delete } from "@vkontakte/icons";
+import { useAlertContext } from "../../../../context/alert-context";
+import { Cards } from "../cards";
+import { IColumn } from "../../interfaces/IColumns";
 import { removeColumn } from "../../actions/columnActions";
+import { useDispatch } from "react-redux";
 
-const mapStateToProps = ({}: RootState): I.StateProps => ({});
+interface ColumnProps {
+  column: IColumn;
+}
 
-const mapDispatchToProps = {
-  removeColumn,
+const Column: React.FC<ColumnProps> = ({ column }) => {
+  const dispatch = useDispatch();
+
+  const { setPopoutHandler, clearPopoutHandler } = useAlertContext();
+
+  const question = useCallback(() => {
+    setPopoutHandler({
+      header: "Внимание",
+      text: `Вы уверены в удалении доски ${column.name}`,
+      actions: [
+        {
+          title: "Да",
+          mode: "destructive",
+          autoclose: true,
+          action: () => dispatch(removeColumn(column)),
+        },
+        {
+          title: "Передумал",
+          mode: "cancel",
+          autoclose: true,
+        },
+      ],
+      actionsLayout: "vertical",
+      onClose: clearPopoutHandler,
+    });
+  }, [clearPopoutHandler, column, dispatch, setPopoutHandler]);
+
+  return (
+    <Group
+      header={
+        <Header mode="secondary" aside={<Icon16Delete onClick={question} />}>
+          {column.name}
+        </Header>
+      }
+    >
+      <Cards columnId={column.id} />
+    </Group>
+  );
 };
 
-const result = connect(mapStateToProps, mapDispatchToProps)(Column);
-
-export { result as Column };
+export { Column };
