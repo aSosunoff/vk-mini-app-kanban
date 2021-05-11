f0faeb0b301dd0ae94c533a5c326ec5ba2954c29
asosunoff <al.sosunoff@gmail.com>
2021-05-11 23:05:38 +0300

    added new selector

--- src/features/columns/selectors.ts
+++ src/features/columns/selectors.ts
@@ -1,6 +1,21 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/redux/reducers";
 import { ICard } from "./interfaces/ICard";
+import { IColumn } from "./interfaces/IColumns";
+
+export const useColumnsSelectors = () => {
+  const columns = useSelector<RootState, IColumn[]>(({ columns }) => {
+    let columnsSelector = [] as IColumn[];
+
+    if (columns) {
+      columnsSelector = Object.entries(columns.column.list).map(([, { column }]) => column);
+    }
+
+    return columnsSelector;
+  });
+
+  return columns;
+};
 
 export const useCardsSelectors = (columnId: string) => {
   const cards = useSelector<RootState, ICard[]>(
@@ -8,4 +23,13 @@ export const useCardsSelectors = (columnId: string) => {
   );
 
   return cards;
+};
+
+export const useCardSelectors = (columnId: string, cardId: string) => {
+  const card = useSelector<RootState, ICard>(
+    ({ columns }) =>
+      columns?.column.list[columnId]?.cards.find((card) => card.id === cardId) ?? ({} as ICard)
+  );
+
+  return card;
 };
