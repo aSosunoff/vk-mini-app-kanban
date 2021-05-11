db6273e611d91a37d482e08fa726b64edeed8724
asosunoff <al.sosunoff@gmail.com>
2021-05-11 23:06:35 +0300

    added new selector for desk

--- src/features/desks/selectors.ts
+++ src/features/desks/selectors.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/redux/reducers";
 import { IDesks } from "./interfaces/IDesks";
@@ -5,4 +6,12 @@ import { IDesks } from "./interfaces/IDesks";
 export const useDesksSelector = () => {
   const desks = useSelector<RootState, IDesks[]>(({ desks }) => desks?.list || []);
   return desks;
+};
+
+export const useDeskSelector = (deskId: string) => {
+  const desks = useDesksSelector();
+
+  const desk = useMemo(() => desks.find(({ id }) => id === deskId), [desks, deskId]);
+
+  return desk;
 };
