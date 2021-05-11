92e65060b092272ce9f43a5d7a7030493e41d08f
asosunoff <al.sosunoff@gmail.com>
2021-05-11 23:05:51 +0300

    fix card

--- src/features/columns/components/card/index.tsx
+++ src/features/columns/components/card/index.tsx
@@ -4,6 +4,9 @@ import { useAlertContext } from "../../../../context/alert-context";
 import { ICard } from "../../interfaces/ICard";
 import { removeCard } from "../../actions/cardActions";
 import { useDispatch } from "react-redux";
+import { Icon24DeleteOutline } from "@vkontakte/icons";
+import { useActivePanel } from "../../../../hooks/useActivePanel";
+import { useRoute } from "react-router5";
 
 interface CardProps {
   card: ICard;
@@ -12,32 +15,44 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ card }) => {
   const dispatch = useDispatch();
 
+  const { goToCard } = useActivePanel();
+
+  const { route } = useRoute();
+
   const { setPopoutHandler, clearPopoutHandler } = useAlertContext();
 
-  const question = useCallback(() => {
-    setPopoutHandler({
-      header: "Внимание",
-      text: `Вы уверены в удалении карточки ${card.name}`,
-      actions: [
-        {
-          title: "Да",
-          mode: "destructive",
-          autoclose: true,
-          action: () => dispatch(removeCard(card)),
-        },
-        {
-          title: "Передумал",
-          mode: "cancel",
-          autoclose: true,
-        },
-      ],
-      actionsLayout: "vertical",
-      onClose: clearPopoutHandler,
-    });
-  }, [setPopoutHandler, card, clearPopoutHandler, dispatch]);
+  const question = useCallback<React.MouseEventHandler<HTMLDivElement>>(
+    (event) => {
+      event.stopPropagation();
+
+      setPopoutHandler({
+        header: "Внимание",
+        text: `Вы уверены в удалении карточки ${card.name}`,
+        actions: [
+          {
+            title: "Да",
+            mode: "destructive",
+            autoclose: true,
+            action: () => dispatch(removeCard(card)),
+          },
+          {
+            title: "Передумал",
+            mode: "cancel",
+            autoclose: true,
+          },
+        ],
+        actionsLayout: "vertical",
+        onClose: clearPopoutHandler,
+      });
+    },
+    [setPopoutHandler, card, clearPopoutHandler, dispatch]
+  );
 
   return (
-    <Cell removable onRemove={question}>
+    <Cell
+      after={<Icon24DeleteOutline onClick={question} />}
+      onClick={() => goToCard(route?.params?.deskId, card.columnId, card.id)}
+    >
       {card.name}
     </Cell>
   );
