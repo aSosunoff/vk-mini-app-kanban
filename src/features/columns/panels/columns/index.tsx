318892467176146823cfbeac49cf09689e9e08dd
asosunoff <al.sosunoff@gmail.com>
2021-05-11 23:06:09 +0300

    fix colemns

--- src/features/columns/panels/columns/index.tsx
+++ src/features/columns/panels/columns/index.tsx
@@ -1,29 +1,69 @@
-import { connect } from "react-redux";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Gallery, Group, Panel, PanelHeader, PanelHeaderBack } from "@vkontakte/vkui";
 
-import { Columns } from "./colums";
-import { RootState } from "../../../../app/redux/reducers";
-import * as I from "./interfaces";
+import { useSnackbarContext } from "../../../../context/snackbar-context";
+import styles from "./columns.module.css";
+import { Column } from "../../components/column";
+import { CreateForm } from "../../../../components/create-form";
+import { useRoute } from "react-router5";
+import { panel } from "../../../../hooks/useActivePanel";
 import { fetchColumns, addedColumns } from "../../actions/columnActions";
-import { IColumn } from "../../interfaces/IColumns";
+import { useDeskSelector } from "../../../desks/selectors";
+import { PanelProps } from "@vkontakte/vkui/dist/components/Panel/Panel";
+import { useColumnsSelectors } from "../../selectors";
 
-const mapStateToProps = ({ desks, columns }: RootState): I.StateProps => {
-  let columnsSelector = [] as IColumn[];
+interface ColumnsProps extends Pick<PanelProps, "id"> {}
 
-  if (columns) {
-    columnsSelector = Object.entries(columns.column.list).map(([, { column }]) => column);
-  }
+const Columns: React.FC<ColumnsProps> = ({ id }) => {
+  const dispatch = useDispatch();
 
-  return {
-    desks: desks?.list ?? [],
-    columns: columnsSelector,
-  };
+  const { snackbar } = useSnackbarContext();
+
+  const { route, router } = useRoute();
+
+  const desk = useDeskSelector(route?.params?.deskId);
+
+  const columns = useColumnsSelectors();
+
+  useEffect(() => {
+    if (route.params.deskId) {
+      dispatch(fetchColumns(route.params.deskId));
+    }
+  }, [route.params.deskId, dispatch]);
+
+  const createColumn = useCallback(
+    async (name: string) => {
+      if (route?.params?.deskId) {
+        await dispatch(addedColumns(route?.params?.deskId, name));
+      }
+    },
+    [dispatch, route?.params?.deskId]
+  );
+
+  return (
+    <Panel id={id} className={styles.columns}>
+      <PanelHeader left={<PanelHeaderBack onClick={() => router.navigate(panel.DESKS)} />}>
+        Доска - {desk?.name}
+      </PanelHeader>
+
+      <Gallery slideWidth="100%" align="center" className={styles.gallery} bullets="dark">
+        {columns.map((column) => (
+          <Column key={column.id} column={column} />
+        ))}
+
+        <Group>
+          <CreateForm
+            onSubmit={createColumn}
+            buttonName="Создать колонку"
+            placeholder="введите название колонки"
+          />
+        </Group>
+      </Gallery>
+
+      {snackbar}
+    </Panel>
+  );
 };
 
-const mapDispatchToProps = {
-  fetchColumns,
-  addedColumns,
-};
-
-const result = connect(mapStateToProps, mapDispatchToProps)(Columns);
-
-export { result as Columns };
+export { Columns };
